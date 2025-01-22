c6d27ae90a5b22de488e669a068b008f639c9179
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2025-01-22 01:27:23 -0300

    refactor: update styles and configuration files; remove unused SVGs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        'background-primary': 'var(--background-primary)',
+        'background-secondary': 'var(--background-secondary)',
+        'background-tertiary': 'var(--background-tertiary)',
+
+        'content-heading': 'var(--content-heading)',
+        'content-body': 'var(--content-body)',
+        'content-headline': 'var(--content-headline)',
+        'content-placeholder': 'var(--content-placeholder)',
+
+        'accent-purple': 'var(--accent-purple)',
+        'accent-blue': 'var(--accent-blue)',
+        'accent-green': 'var(--accent-green)',
+        'accent-pink': 'var(--accent-pink)',
+        'accent-yellow': 'var(--accent-yellow)',
+
+        'border-primary': 'var(--border-primary)',
+        'border-secondary': 'var(--border-secondary)',
+        'border-tertiary': 'var(--border-tertiary)'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
